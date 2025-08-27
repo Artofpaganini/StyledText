08bce32073fc4e5568aaa2a7cd1c5fb70dd59b78
dobrov.v <dobrov.v@promail366.com>
2025-08-27 16:04:27 +0300

    3-d commit

--- app/src/main/java/io/github/artofpaganini/styledtextsampe/MainActivity.kt
+++ app/src/main/java/io/github/artofpaganini/styledtextsampe/MainActivity.kt
@@ -6,15 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
@@ -23,15 +28,19 @@ import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.PlatformParagraphStyle
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextDecoration.Companion.Underline
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import io.github.artofpaganini.styled_text.builders.container.styledTextContainer
+import io.github.artofpaganini.styled_text.builders.paragraph_style.paragraphStyle
 import io.github.artofpaganini.styled_text.builders.span_style.spanStyle
 import io.github.artofpaganini.styled_text.builders.text.styledText
 import io.github.artofpaganini.styledtextsampe.ui.theme.Purple40
@@ -39,15 +48,7 @@ import io.github.artofpaganini.styledtextsampe.ui.theme.StyledTextSampeTheme
 import io.github.artofpaganini.styledtextsampe.ui.theme.gradientColors1
 import io.github.artofpaganini.styledtextsampe.ui.theme.gradientColors2
 
-//paragraphStyle = paragraphStyle {
-//    textAlign = TextAlign.End
-//    textIndent = TextIndent(firstLine = 8.sp, restLine = 30.sp)
-//    platformStyle = PlatformParagraphStyle(includeFontPadding = false)
-//    lineHeightStyle = LineHeightStyle(
-//        alignment = LineHeightStyle.Alignment.Center,
-//        trim = LineHeightStyle.Trim.None
-//    )
-//}
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,11 +56,10 @@ class MainActivity : ComponentActivity() {
         val inlineId = "imageId"
         val inlineContent = mapOf(
             inlineId to InlineTextContent(
-                Placeholder(20.sp, 20.sp, PlaceholderVerticalAlign.TextCenter)
+                Placeholder(40.sp, 40.sp, PlaceholderVerticalAlign.TextCenter)
             ) {
                 Image(
-                    imageVector = Icons.Default.Call,
-//                    modifier = Modifier.size(42.dp),
+                    imageVector = Icons.Default.Person,
                     contentDescription = ""
                 )
             }
@@ -71,7 +71,7 @@ class MainActivity : ComponentActivity() {
                     gradientColors1,
                     Offset(0.0f, 0.5f)
                 )
-                fontWeight = FontWeight.Bold
+                fontWeight = Bold
             }
         }
         val gitHubString = styledTextContainer {
@@ -85,7 +85,7 @@ class MainActivity : ComponentActivity() {
             styledText {
                 text = " is my "
                 decorStyle = spanStyle {
-                    color = Color.Yellow
+                    color = Color.Cyan
                     baselineShift = BaselineShift.Subscript
                 }
             }
@@ -100,7 +100,7 @@ class MainActivity : ComponentActivity() {
                     textDecoration = TextDecoration.None
                 }
                 hoveredStyle = spanStyle {
-                    color = Color.Yellow
+                    color = Color.LightGray
                     textDecoration = Underline
                 }
                 pressedStyle = spanStyle {
@@ -168,82 +168,39 @@ class MainActivity : ComponentActivity() {
 
         val textWithIcon: AnnotatedString = styledTextContainer {
             styledText {
-                text = "New text where symbols from 7 till 12 have Red color, and other have brush with gradient "
+                text = "Text where icon here, and also in the end"
                 startIndex = 7
                 endIndex = 12
                 decorStyle = spanStyle {
                     color = Color.Red
                 }
                 inlineContentId = inlineId
-                inlineContentIndex = 5
+                inlineContentIndex = 20
 
                 outerDecorStyle = spanStyle {
                     brush = Brush.linearGradient(
                         gradientColors2,
                         Offset(0.0f, 0.5f)
                     )
-                    fontWeight = FontWeight.Bold
+                    fontWeight = Bold
                 }
             }
             inlineContent {
                 id = inlineId
             }
         }
-        val a = styledTextContainer {
 
-            styledText {
-                text = " "
-            }
-            styledUrl {
-                urlTitle = "AndroidDev"
-
-                decorStyle = spanStyle {
-                    color = Color.Green
-                }
-                focusedStyle = spanStyle {
-                    background = Color(0xFF1565C0)
-                    textDecoration = Underline
-                }
-                hoveredStyle = spanStyle {
-                    color = Color.Yellow
-                    textDecoration = Underline
-                }
-                pressedStyle = spanStyle {
-                    color = Color.Red
-                    textDecoration = TextDecoration.LineThrough
-                }
-            }
-
-            styledText {
-                text = "      "
-            }
-            styledText {
-                text = "123"
-                decorStyle = spanStyle {
-                    color = Color.Blue
-                    textDecoration = TextDecoration.LineThrough
-                }
-            }
-            styledText {
-                text = "123"
-                decorStyle = spanStyle {
-                    color = Color.Red
-                    textDecoration = Underline
-                }
-            }
-            styledText {
-                text = " "
-            }
-
-            styledUrl {
-                urlTitle = "AndroidDev"
-                url = "https://developer.android.com/develop/ui/compose/quick-guides/content/support-multiple-links"
-                decorStyle = spanStyle {
-                    brush = Brush.linearGradient(
-                        gradientColors1,
-                        Offset(0.0f, 0.5f)
-                    )
-                }
+        val textWithParagraphSettings: AnnotatedString = styledText {
+            text = "Text where paragraph ended after this word. Also with some others words"
+            startIndex = 43
+            paragraphStyle = paragraphStyle {
+                textAlign = TextAlign.End
+                platformStyle = PlatformParagraphStyle(includeFontPadding = false)
+                lineBreak = LineBreak.Heading
+                lineHeightStyle = LineHeightStyle(
+                    alignment = LineHeightStyle.Alignment.Top,
+                    trim = LineHeightStyle.Trim.None
+                )
             }
         }
 
@@ -255,36 +212,54 @@ class MainActivity : ComponentActivity() {
                         .padding(20.dp)
                         .fillMaxSize()
                         .safeContentPadding(),
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
                 ) {
                     val textModifier = Modifier
                         .fillMaxWidth()
-                    Text(
+                    StyledText(
                         modifier = textModifier,
                         text = title,
+                        alignment = Alignment.Center,
                     )
-                    Text(
+                    StyledText(
                         modifier = textModifier,
                         text = gitHubString,
                     )
-
-                    Text(
+                    StyledText(
                         modifier = textModifier,
                         text = textWithDifferentStyles,
                     )
-                    Text(
+                    StyledText(
                         modifier = textModifier,
                         text = textWithDifferentColor,
                     )
-
-                    Text(
+                    StyledText(
                         modifier = textModifier,
                         inlineContent = inlineContent,
                         text = textWithIcon,
                     )
 
+                    StyledText(
+                        modifier = textModifier,
+                        text = textWithParagraphSettings,
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun StyledText(
+    modifier: Modifier = Modifier,
+    text: AnnotatedString,
+    alignment: Alignment = Alignment.CenterStart,
+    inlineContent: Map<String, InlineTextContent> = mapOf(),
+) {
+    Box(modifier = modifier, contentAlignment = alignment) {
+        Text(
+            inlineContent = inlineContent,
+            text = text,
+        )
     }
 }
